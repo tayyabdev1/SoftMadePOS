fcd8bb64a72cf65ca88b83ac80971249eebed7e6
TAYYAB <tmirza217@gmail.com>
2026-01-05 11:17:20 +0500

    DB Backup

--- Program.cs
+++ Program.cs
@@ -13,6 +13,12 @@ builder.Services.AddDbContextFactory<AppDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -21,12 +27,28 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+if (!app.Environment.IsDevelopment())
+{
+    Task.Run(() => {
+    System.Threading.Thread.Sleep(1000);
+    try
+    {
+        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+        {
+            FileName = "http://localhost:5000",
+            UseShellExecute = true
+        });
+    }
+        catch { }
+    });
+}
 
 app.Run();
